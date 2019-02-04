411c9d84f44b310a58010598062e9b741824be6b
nubianMONK <gnosisdynamcics@gmail.com>
2019-02-04 00:44:41 -0500

    Setup separate test and development environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 
 const _ = require('lodash');
 const {ObjectID} = require('mongodb');
@@ -9,7 +10,7 @@ let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
 let app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
